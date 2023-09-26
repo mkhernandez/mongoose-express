58b3f3edede003e5f8336539a5efb4df7b8e4829
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-26 16:44:01 -0500

    Initial setup of express, view engine as ejs, and mongoose connection to mongo

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const app = express();
+const port = 5000;
+const localhost = "localhost";
+const log = console.log;
+
+// Connection to mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/shopApp')// <= In here 127.0.0.1 because error thrown with "localhost"
+.then(() => {
+    log("MONGO CONNECTION OPEN!");
+})
+.catch(err => {
+    log("OH NO, MONGO CONNECTION ERROR!");
+    log(err);
+})
+
+// Set the view engine and views path
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// ROUTES 
+// These will be moved eventually into their own file.
+app.get("/dogs", (req, res) => {
+    res.send("WOOF!");
+});
+
+app.listen(port, localhost, () => {
+    log(`Listening on http://${localhost}: ${port}`);
+});
