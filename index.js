2d0ead4c2041ca97a78bcfcacb80bef1f226766d
Mark Hernandez <markkimhernandez@gmail.com>
2023-09-27 11:45:04 -0500

    Added get route for new product form and post route to add product

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')// <= In here 127.0.0.1 b
 // Set the view engine and views path
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// Body parser 
+app.use(express.urlencoded({extended: true}));
 
 // ROUTES 
 // These will be moved eventually into their own file.
@@ -28,6 +30,18 @@ app.get("/products", async (req, res) => {
     const products = await Product.find({});
     log(products);
     res.render("products/index", { products });
+});
+
+// GET new product form route
+app.get('/products/new', (req, res) => {
+    res.render("products/new");
+});
+
+// POST ROUTE to add a new product
+app.post("/products", async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
 });
 
 // SHOW DETAILS OF ONE PRODUCT
